56e3c1d5fc352056535e55ba9fefa615d7d784f8
lzx <486877184@qq.com>
2025-10-23 15:25:38 +0800

    添加全局异常

--- src/main/java/com/atguigu/exam/common/GlobalExceptionHandler.java
+++ src/main/java/com/atguigu/exam/common/GlobalExceptionHandler.java
@@ -1,4 +1,18 @@
 package com.atguigu.exam.common;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(value = Exception.class)
+    public Result exception(Exception e){
+        e.printStackTrace();
+        //记录异常的日志
+        log.error("服务器发生运行时异常，异常信息为{}",e.getMessage());
+        return Result.error(e.getMessage());
+    }
 }
