75ea858aa419eefce6ba068c78e3b3e7ad14c8cc
lzx <486877184@qq.com>
2025-10-24 17:50:12 +0800

    引入redis缓存jar包 redis的配置 封装redis的操作

---
+++ src/main/java/com/atguigu/exam/config/RedisConfig.java
@@ -0,0 +1,24 @@
+package com.atguigu.exam.config;
+
+import com.alibaba.fastjson.support.spring.GenericFastJsonRedisSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+    @Bean
+    public RedisTemplate<String,Object> redisTemplate(RedisConnectionFactory connectionFactory){
+        RedisTemplate<String,Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new GenericFastJsonRedisSerializer());
+        template.setHashKeySerializer(new StringRedisSerializer());
+        template.setHashValueSerializer(new GenericFastJsonRedisSerializer());
+        template.afterPropertiesSet();
+        return template;
+    }
+
+}
